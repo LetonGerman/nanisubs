21d6d56072abe7d12272d508d61700325eb89fc0
Project 17 <neveraskedfor@gmail.com>
2019-06-06 23:49:00 +0300

    Initial commit

---
+++ src/components/Gallery.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+ <div class="pepes">
+  <img class="pepe" v-for="(pepe, index) in images" v-bind:key="index" v-bind:src="pepe">
+   <p>{{ pepe }}</p>
+ </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallery',
+  data: function () {
+    return {
+      publicPath: process.env.BASE_URL,
+      images:
+        ['/static/foszki.png', '/static/hereza.png', '/static/hope.png', '/static/port80se.png', '/static/dante.png']
+    }
+  }
+}
+</script>
+
+<style scoped>
+  body {
+    background-color: black;
+  }
+
+  .pepes {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .pepe {
+    flex: 0 0 25%;
+  }
+</style>
