be49f30998776b74dafd7bd64b9be415a43fb731
bl4nkfce <bl4nkfce@gmail.com>
2019-06-28 00:28:43 +0200

    firestore trigger for automatic file removal

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,8 +1,37 @@
-const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const serviceAccount = require('./serviceAccount.json')
+const firebaseConfig = require('./firebaseConfig')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: firebaseConfig.databaseURL,
+  storageBucket: firebaseConfig.storageBucket
+})
+exports.removeLinkedFiles = functions.firestore
+  .document('pepes/{pepeID}')
+  .onDelete((snap, context) => {
+    const { tempId } = snap.data()
+    if (tempId !== null && tempId !== undefined) {
+      return admin
+        .storage()
+        .bucket()
+        .getFiles({ directory: 'pepes/' })
+        .then(data => {
+          return data[0].find(
+            file => file.name.replace('pepes/', '') === tempId
+          )
+        })
+        .then(fileToRemove => {
+          return fileToRemove.delete()
+        })
+        .then(data => {
+          console.log('successfully removed a file named ' + tempId)
+          return data
+        })
+        .catch(error => {
+          return console.error(error)
+        })
+    } else {
+      return null
+    }
+  })
